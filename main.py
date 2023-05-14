7d779bbac612755d83a60cbdca05ec11c6ca0498
mletizia <marco.letizia@edu.unige.it>
2023-05-14 12:56:41 +0000

    small fixes

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ weight=N1/N0
 reference_path = "/data/marcol/DQM/6D/reference/RUN000085_events.csv"
 data_paths = {
                 "Thr 75%": "/data/marcol/DQM/6D/thresholds/thresholds_75/RUN000086_events.csv",
-                #"Thr 25%": "/data/marcol/DQM/6D/thresholds/thresholds_25/RUN000088_events.csv",
-                #"Thr 50%": "/data/marcol/DQM/6D/thresholds/thresholds_50/RUN000087_events.csv",
-                #"Ca 25%": "/data/marcol/DQM/6D/cathods/cathods_25/RUN000091_events.csv",
-                #"Ca 50%": "/data/marcol/DQM/6D/cathods/cathods_50/RUN000090_events.csv",
-                #"Ca 75%": "/data/marcol/DQM/6D/cathods/cathods_75/RUN000089_events.csv",
+                "Thr 25%": "/data/marcol/DQM/6D/thresholds/thresholds_25/RUN000088_events.csv",
+                "Thr 50%": "/data/marcol/DQM/6D/thresholds/thresholds_50/RUN000087_events.csv",
+                "Ca 25%": "/data/marcol/DQM/6D/cathods/cathods_25/RUN000091_events.csv",
+                "Ca 50%": "/data/marcol/DQM/6D/cathods/cathods_50/RUN000090_events.csv",
+                "Ca 75%": "/data/marcol/DQM/6D/cathods/cathods_75/RUN000089_events.csv",
             }
 
 output_dir="./runs/"+datetime.now().strftime("%d%M%Y_%H%m%S")+f"/{len(features)}D_{N0}_{N1}/ref"
@@ -43,14 +43,9 @@ flk_config = get_logflk_config(M,flk_sigma,[lam],weight=weight,iter=[1000],seed=
 
 run_toys_2(reference, reference, "Ref", output_dir+"/t_ref.txt", N0, N1, rng,  flk_config, n_toys=5, std='scaler', p=None, replacement=True, plt_freq=5, plot_dir=output_dir+"/plot_ref")
 
-#run_toys(reference, reference, output_dir+"/t_ref.txt", N0, N1, rng, flk_config, n_toys=100, std='scaler', replacement=True, plt_freq=25, plot_dir=output_dir+"/plot_ref/", data_label="Ref")
-
-
 
 for key,value in data_paths.items():
     print("[--] DATA: "+key)
     data = read_data(value,features=features,rnd=rng)
     new_output_dir=os.path.dirname(output_dir)+"/"+key
     run_toys_2(reference, data, key, new_output_dir+"/t_data.txt", N0, N1, rng,  flk_config, n_toys=5, std='scaler', p=None, replacement=False, plt_freq=2, plot_dir=new_output_dir+"/plot_data")
-    #run_toys(reference, data, new_output_dir+"/t_data.txt", N0, N1, rng, flk_config, n_toys=100, std='scaler', replacement=False, plt_freq=3, plot_dir=new_output_dir+"/plot_data/", data_label=key)
-
